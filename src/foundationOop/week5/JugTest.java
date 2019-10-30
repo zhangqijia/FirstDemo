c2077f756889dde44e4e42123280211fbdbe034b
zhang <zhang5967611@163.com>
2019-10-30 13:12:13 +0000

    week5

--- src/foundationOop/week5/JugTest.java
+++ src/foundationOop/week5/JugTest.java
@@ -1,4 +1,25 @@
 package foundationOop.week5;
 
 public class JugTest {
+
+    public static void main(String[] args) {
+        Jug jug3 = new Jug(3);
+        Jug jug2 = new Jug(2);
+        Jug jug5 = new Jug(5);
+
+        jug2.fill();
+        jug3.fill();
+
+        jug2.pourInto(jug5);
+        jug3.pourInto(jug5);
+
+        jug5.display();
+
+        jug5.pourInto(jug2);
+        jug5.display();
+
+        jug5.pourInto(jug3);
+        jug5.display();
+
+    }
 }
