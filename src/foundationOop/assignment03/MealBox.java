fa0d1facffd941290213f5f1fb997238b7ec9c7a
zhangqijia <zhangqijia@corp.netease.com>
2019-12-05 12:26:02 +0000

    MealBox

---
+++ src/foundationOop/assignment03/MealBox.java
@@ -0,0 +1,12 @@
+package foundationOop.assignment03;
+
+import foundationOop.assignment03.drink.Drink;
+import foundationOop.assignment03.food.Meal;
+
+public class MealBox {
+
+    private Meal meal;
+    private Drink drink;
+}
+
+
---
+++ src/foundationOop/assignment03/food/Meal.java
@@ -0,0 +1,9 @@
+package foundationOop.assignment03.food;
+
+/**
+ * Meal is the superclass of all dishes, such as Pie, Fish, Steak
+ */
+abstract public class Meal {
+
+
+}
